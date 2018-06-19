9d0a949750fb81e1f86ae6b87592f648bf1ae34c
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2018-06-19 15:17:03 +0900

    sidekiq のジョブ数監視に対応

--- lib/jobmon/railtie.rb
+++ lib/jobmon/railtie.rb
@@ -4,6 +4,7 @@ module Jobmon
       require 'jobmon/tasks/preload'
       load 'jobmon/tasks/test.rake'
       load 'jobmon/tasks/delayed_job_queue_monitor.rake'
+      load 'jobmon/tasks/sidekiq_queue_monitor.rake'
     end
   end
 end
