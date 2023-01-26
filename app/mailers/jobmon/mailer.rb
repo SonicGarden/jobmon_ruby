3aadb5a3b7fd91ade4a75d9f5f83df79eaf38265
Takahiro Chiba <cbtkhr125@gmail.com>
2023-01-26 15:21:13 +0900

    feat: Mailerの親クラスを parent_mailer で指定可能にする

--- app/mailers/jobmon/mailer.rb
+++ app/mailers/jobmon/mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if defined?(ActionMailer)
-  class Jobmon::Mailer < ApplicationMailer
+  class Jobmon::Mailer < Jobmon.configuration.parent_mailer.constantize
     default from: Jobmon.configuration.from_email if Jobmon.configuration.from_email
 
     def healthcheck
