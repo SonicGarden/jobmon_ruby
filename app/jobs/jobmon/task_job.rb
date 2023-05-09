2c0fc504624465f02b218e19e8c854fb6fc8eef7
aki <lala.akira@gmail.com>
2023-05-09 16:12:51 +0900

    feat: Add Jobmon::TaskJob

---
+++ app/jobs/jobmon/task_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# ActiveJobバックエンドのスケジューラからの呼び出し用
+class Jobmon::TaskJob < ActiveJob::Base
+  queue_as Jobmon.configuration.default_task_job_queue
+
+  # TODO: test
+  def perform(task:, estimate_time: 60, name: nil)
+    options = {
+      estimate_time:,
+      name:,
+    }.compact.map { |k, v| "--#{k.to_s.dasherize} #{v}" }.join(' ')
+
+    Kernel.system(
+      "bundle exec jobmon #{options} #{task}",
+      exception: true,
+      chdir: Rails.root.to_s
+    )
+  end
+end
