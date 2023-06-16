6115b5b8a51faf116cc82a5b7e20557c0833b027
aki <lala.akira@gmail.com>
2023-06-16 16:00:21 +0900

    test: Add failure spec

--- spec/jobmon/jobs/task_job_spec.rb
+++ spec/jobmon/jobs/task_job_spec.rb
@@ -7,38 +7,50 @@ ActiveJob::Base.logger = ActiveSupport::Logger.new(nil)
 
 describe Jobmon::TaskJob do
   describe '#perform' do
-    before do
-      allow(Open3).to receive(:capture3).and_return(['', '', double(success?: true)])
+    context 'success' do
+      before do
+        allow(Open3).to receive(:capture3).and_return(['', '', double(success?: true)])
+      end
+
+      it 'run task' do
+        Jobmon::TaskJob.perform_now(task: 'test')
+        expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', 'test', anything)
+      end
+
+      it 'run task with env' do
+        Jobmon::TaskJob.perform_now(task: 'test ID=1')
+        expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', 'test', 'ID=1', anything)
+      end
+
+      it 'run task with args' do
+        Jobmon::TaskJob.perform_now(task: 'test "hello world"')
+        expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', 'test', 'hello world', anything)
+      end
+
+      it 'run task with estimate_time options' do
+        Jobmon::TaskJob.perform_now(task: 'test', estimate_time: 30)
+        expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', '--estimate-time', '30', 'test', anything)
+      end
+
+      it 'run task with name options' do
+        Jobmon::TaskJob.perform_now(task: 'test', name: 'dummy')
+        expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', '--name', 'dummy', 'test', anything)
+      end
+
+      it 'run task with estimate_time and name options' do
+        Jobmon::TaskJob.perform_now(task: 'test', estimate_time: 30, name: 'dummy')
+        expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', '--estimate-time', '30', '--name', 'dummy', 'test', anything)
+      end
     end
 
-    it 'run task' do
-      Jobmon::TaskJob.perform_now(task: 'test')
-      expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', 'test', anything)
-    end
+    context 'failure' do
+      before do
+        allow(Open3).to receive(:capture3).and_return(['', 'error', double(success?: false)])
+      end
 
-    it 'run task with env' do
-      Jobmon::TaskJob.perform_now(task: 'test ID=1')
-      expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', 'test', 'ID=1', anything)
-    end
-
-    it 'run task with args' do
-      Jobmon::TaskJob.perform_now(task: 'test "hello world"')
-      expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', 'test', 'hello world', anything)
-    end
-
-    it 'run task with estimate_time options' do
-      Jobmon::TaskJob.perform_now(task: 'test', estimate_time: 30)
-      expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', '--estimate-time', '30', 'test', anything)
-    end
-
-    it 'run task with name options' do
-      Jobmon::TaskJob.perform_now(task: 'test', name: 'dummy')
-      expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', '--name', 'dummy', 'test', anything)
-    end
-
-    it 'run task with estimate_time and name options' do
-      Jobmon::TaskJob.perform_now(task: 'test', estimate_time: 30, name: 'dummy')
-      expect(Open3).to have_received(:capture3).with('bundle', 'exec', 'jobmon', '--estimate-time', '30', '--name', 'dummy', 'test', anything)
+      it 'raise Jobmon::TaskJobError' do
+        expect { Jobmon::TaskJob.perform_now(task: 'test') }.to raise_error(Jobmon::TaskJobError)
+      end
     end
   end
 end
